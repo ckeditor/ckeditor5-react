1de62b2fedbf66201792c2801f6b81bf26a76789
Kamil Piechaczek <pomek@pomek.pl>
2018-07-18 14:26:21 +0200

    Added npm scripts for releasing.

---
+++ scripts/release.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+
+/**
+ * @license Copyright (c) 2003-2018, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md.
+ */
+
+'use strict';
+
+require( '@ckeditor/ckeditor5-dev-env' ).releaseRepository();
