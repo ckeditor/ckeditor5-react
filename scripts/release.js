f751990562d66633f56cae68a7c5e34b5c594f09
Kamil Piechaczek <pomek@pomek.pl>
2018-07-25 07:35:36 +0200

    Internal: Changed a script for releasing the package.

--- scripts/release.js
+++ scripts/release.js
@@ -5,36 +5,134 @@
  * For licensing, see LICENSE.md.
  */
 
-'use strict';
-
 /* eslint-env node */
 
-/**
- * Scripts for releasing the package.
- *
- * Before starting the process, "devDependencies" key is removed from package.json.
- */
+'use strict';
 
 const path = require( 'path' );
-const { tools } = require( '@ckeditor/ckeditor5-dev-utils' );
+const { tools, logger } = require( '@ckeditor/ckeditor5-dev-utils' );
+const versionUtils = require( '@ckeditor/ckeditor5-dev-env/lib/release-tools/utils/versions' );
+const cli = require( '@ckeditor/ckeditor5-dev-env/lib/release-tools/utils/cli' );
+const createGithubRelease = require( '@ckeditor/ckeditor5-dev-env/lib/release-tools/utils/creategithubrelease' );
+const validatePackageToRelease = require( '@ckeditor/ckeditor5-dev-env/lib/release-tools/utils/validatepackagetorelease' );
+const { getChangesForVersion } = require( '@ckeditor/ckeditor5-dev-env/lib/release-tools/utils/changelog' );
 
-const packageJsonPath = path.resolve( 'package.json' );
-const originalPackageJson = require( packageJsonPath );
+const log = logger();
+const packageRoot = path.resolve( __dirname, '..' );
+const packageJsonPath = path.resolve( packageRoot, 'package.json' );
+const templatePath = path.resolve( packageRoot, 'scripts', 'template' );
+const packageJsonTemplatePath = path.resolve( templatePath, 'package.json' );
+const packageJsonTemplateCopy = require( packageJsonTemplatePath );
 
-Promise.resolve()
-	.then( () => {
-		tools.updateJSONFile( packageJsonPath, json => {
-			delete json.devDependencies;
+// That files will be copied from source to template directory and will be released too.
+const additionalFiles = [
+	'CHANGELOG.md',
+	'LICENSE.md',
+	'README.md',
+	'dist'
+];
 
-			return json;
+cli.provideToken()
+	.then( token => {
+		const gitVersion = versionUtils.getLastTagFromGit();
+		const changelogVersion = versionUtils.getLastFromChangelog();
+
+		log.info( 'Checking whether there is anything to release...' );
+
+		// If the last tag is equal to version saved in changelog, we don't have new version for release.
+		if ( gitVersion === changelogVersion ) {
+			return reject( 'Before starting the release process, you should generate the changelog.' );
+		}
+
+		const releaseDescription = getChangesForVersion( changelogVersion );
+
+		log.info( 'Validating the repository for the release...' );
+
+		const errors = validatePackageToRelease( {
+			version: changelogVersion,
+			changes: releaseDescription
 		} );
-	} )
-	.then( () => {
-		return require( '@ckeditor/ckeditor5-dev-env' ).releaseRepository();
-	} )
-	.then( () => {
-		tools.updateJSONFile( packageJsonPath, () => {
-			return originalPackageJson;
+
+		// Abort due to errors during validation.
+		if ( errors.length ) {
+			const log = logger();
+
+			log.error( 'Unexpected errors occured:' );
+			errors.map( err => '* ' + err ).forEach( log.error.bind( log ) );
+
+			return reject( 'Releasing has been aborted due to errors.' );
+		}
+
+		log.info( 'Bumping the version...' );
+
+		// Bump the version.
+		tools.shExec( `npm version ${ changelogVersion } --message "Release: v${ changelogVersion }."`, { verbosity: 'error' } );
+
+		// Push the release to remote.
+		tools.shExec( `git push origin master v${ changelogVersion }` );
+
+		const packageJson = require( packageJsonPath );
+
+		log.info( 'Copying the package.json...' );
+
+		// Update the template of `package.json`. We will use values from source `package.json`
+		// but only these ones which are defined in the template.
+		tools.updateJSONFile( packageJsonTemplatePath, jsonFile => {
+			for ( const property of Object.keys( jsonFile ) ) {
+				// If the `property` is set in the template, leave it.
+				if ( jsonFile[ property ] ) {
+					continue;
+				}
+
+				if ( property == 'private' ) {
+					continue;
+				}
+
+				// Copy value from original package.json file.
+				jsonFile[ property ] = packageJson[ property ];
+			}
+
+			return jsonFile;
 		} );
+
+		// Copy additional files.
+		for ( const file of additionalFiles ) {
+			tools.shExec( `cp -R ${ path.resolve( packageRoot, file ) } ${ path.resolve( templatePath, file ) }` );
+		}
+
+		log.info( 'Publishing on npm...' );
+
+		// Publish the package on npm.
+		tools.shExec( `cd ${ templatePath } && npm publish && cd ${ packageRoot }` );
+
+		// Remove files that were copy.
+		for ( const file of additionalFiles ) {
+			tools.shExec( `rm -rf ${ path.resolve( templatePath, file ) }` );
+		}
+
+		log.info( 'Creating a release on GitHub...' );
+
+		// Create a release on GitHub.
+		return createGithubRelease( token, {
+			repositoryOwner: 'ckeditor',
+			repositoryName: 'ckeditor5-react',
+			version: `v${ changelogVersion }`,
+			description: releaseDescription
+		} ).then( () => changelogVersion );
+	} )
+	.then( version => {
+		log.info( 'Restoring the package.json template...' );
+
+		// Restore the template `package.json` to state before the publishing process.
+		tools.updateJSONFile( packageJsonTemplatePath, () => packageJsonTemplateCopy );
+
+		const url = `https://github.com/ckeditor/ckeditor5-react/releases/tag/v${ version }`;
+		log.info( `Created the release: ${ url }` );
+	} )
+	.catch( err => {
+		log.error( err.stack );
 	} );
 
+function reject( message ) {
+	return Promise.reject( new Error( message ) );
+}
