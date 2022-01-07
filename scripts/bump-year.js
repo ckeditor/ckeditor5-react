cf54b33604523ef48d892525f3febfe48f282ee1
Przemyslaw Zan <p.zan@cksource.com>
2022-01-07 16:29:16 +0100

    Added `bump-year` script.

---
+++ scripts/bump-year.js
@@ -0,0 +1,34 @@
+#!/usr/bin/env node
+
+/**
+ * @license Copyright (c) 2003-2021, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md.
+ */
+
+/* eslint-env node */
+
+/*
+
+Usage:
+node scripts/bump-year.js
+
+And after reviewing the changes:
+git commit -am "Internal: Bumped the year." && git push
+
+*/
+
+require( '@ckeditor/ckeditor5-dev-env' )
+	.bumpYear( {
+		cwd: process.cwd(),
+		globPatterns: [
+			{ // LICENSE.md, .eslintrc.js, etc.
+				pattern: '*',
+				options: {
+					dot: true
+				}
+			},
+			{
+				pattern: '!(coverage|.nyc_output|dist)/**'
+			}
+		]
+	} );
