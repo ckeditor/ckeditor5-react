60f05d42f22a56f2d5d1c22daa55fdc9b564f6ea
Marcin Panek <m.panek@cksource.com>
2022-12-20 17:10:38 +0100

    Use `ckeditor5-dev-ci-notify-travis-status` script and remove `ckeditor5-dev-env`

--- scripts/publish.js
+++ scripts/publish.js
@@ -7,7 +7,7 @@
 
 'use strict';
 
-require( '@ckeditor/ckeditor5-dev-env' )
+require( '@ckeditor/ckeditor5-dev-release-tools' )
 	.releaseSubRepositories( {
 		cwd: process.cwd(),
 		packages: null,
