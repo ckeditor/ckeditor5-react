c20e255cec83dbf25cf187ef5f8a350ff4d2b3c2
Kamil Piechaczek <pomek@users.noreply.github.com>
2023-03-29 11:24:22 +0200

    Internal: Support for defining npm tag.

--- scripts/publish.js
+++ scripts/publish.js
@@ -7,9 +7,18 @@
 
 'use strict';
 
+const minimist = require( 'minimist' );
+
+const argv = minimist( process.argv.slice( 2 ), {
+	string: [
+		'npm-tag'
+	]
+} );
+
 require( '@ckeditor/ckeditor5-dev-release-tools' )
 	.releaseSubRepositories( {
 		cwd: process.cwd(),
 		packages: null,
+		npmTag: argv[ 'npm-tag' ],
 		dryRun: process.argv.includes( '--dry-run' )
 	} );
