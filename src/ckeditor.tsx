5c4477d375466bbc43ef40ecff63f0f3a1ae2a67
Mateusz Baginski <cziken58@gmail.com>
2024-07-10 14:27:22 +0200

    Revert unecessary change

--- src/ckeditor.tsx
+++ src/ckeditor.tsx
@@ -254,7 +254,7 @@ export default class CKEditor<TEditor extends Editor> extends React.Component<Pr
 
 			const instance = await this._createEditor( el as any, config );
 
-			if ( editorSemaphore && totalRestartsRef.current > 0 ) {
+			if ( totalRestartsRef.current > 0 ) {
 				editorSemaphore!.unsafeSetValue( {
 					instance,
 					watchdog
