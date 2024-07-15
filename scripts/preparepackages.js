517907574772265d64cf358dff2254cf672b4ccf
Kamil Piechaczek <pomek@users.noreply.github.com>
2024-07-15 10:04:08 +0200

    Read the package.json file after bumping the package version.

--- scripts/preparepackages.js
+++ scripts/preparepackages.js
@@ -15,7 +15,6 @@ import parseArguments from './utils/parsearguments.js';
 import getListrOptions from './utils/getlistroptions.js';
 
 const require = createRequire( import.meta.url );
-const packageJson = require( '../package.json' );
 
 const latestVersion = releaseTools.getLastFromChangelog();
 const versionChangelog = releaseTools.getChangesForVersion( latestVersion );
@@ -74,7 +73,7 @@ const tasks = new Listr( [
 		task: () => {
 			return releaseTools.prepareRepository( {
 				outputDirectory: 'release',
-				rootPackageJson: packageJson
+				rootPackageJson: require( '../package.json' )
 			} );
 		}
 	},
