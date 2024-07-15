1a92627bad428947e4c52ce3b8a4ee2a0c5d51a7
Kamil Piechaczek <pomek@users.noreply.github.com>
2024-07-15 10:03:59 +0200

    ESM issues.

--- scripts/changelog.js
+++ scripts/changelog.js
@@ -8,7 +8,7 @@
 /* eslint-env node */
 
 import { generateChangelogForSinglePackage } from '@ckeditor/ckeditor5-dev-release-tools';
-import parseArguments from './utils/parsearguments';
+import parseArguments from './utils/parsearguments.js';
 
 const cliArguments = parseArguments( process.argv.slice( 2 ) );
 
