a3c321be3b6514004f3291acf3887a4ed8364901
Marcin Panek <m.panek@cksource.com>
2024-07-16 13:13:18 +0200

    Support for prereleases released via CI.

--- scripts/ci/is-project-ready-to-release.js
+++ scripts/ci/is-project-ready-to-release.js
@@ -11,37 +11,19 @@
 
 import { createRequire } from 'module';
 import releaseTools from '@ckeditor/ckeditor5-dev-release-tools';
-import { execSync } from 'child_process';
-import semver from 'semver';
 
 const require = createRequire( import.meta.url );
-const { name } = require( '../../package.json' );
+const { name: packageName } = require( '../../package.json' );
 
-const latestPublishedVersion = execSync( `npm view ${ name }@latest version`, { encoding: 'utf-8' } ).trim();
 const changelogVersion = releaseTools.getLastFromChangelog();
+const npmTag = releaseTools.getNpmTagFromVersion( changelogVersion );
 
-if ( getVersionTag( changelogVersion ) !== 'latest' ) {
-	console.log( `Aborting due non-latest changelog version (${ changelogVersion }).` );
-	process.exit( 1 );
-}
-
-if ( semver.lte( changelogVersion, latestPublishedVersion ) ) {
-	console.log(
-		`The proposed changelog (${ changelogVersion }) version is not greater than the published one (${ latestPublishedVersion }).`
-	);
-	process.exit( 1 );
-}
-
-console.log( 'The project is ready to release.' );
-
-/**
- * Returns an npm tag based on the specified release version.
- *
- * @param {String} version
- * @returns {String}
- */
-function getVersionTag( version ) {
-	const [ versionTag ] = semver.prerelease( version ) || [ 'latest' ];
-
-	return versionTag;
-}
+releaseTools.isVersionPublishableForTag( packageName, changelogVersion, npmTag )
+	.then( result => {
+		if ( !result ) {
+			console.error( `The proposed changelog (${ changelogVersion }) version is not higher than the already published one.` );
+			process.exit( 1 );
+		} else {
+			console.log( 'The project is ready to release.' );
+		}
+	} );
