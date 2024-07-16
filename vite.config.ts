6dfcde03652cf99808f03fc54438f0f91c547b06
Filip Sobol <filipsobol@users.noreply.github.com>
2024-07-16 15:06:58 +0200

    Fix: Change the global name used in the UMD build from CKEditor to CKEDITOR_REACT to match the new convention.

--- vite.config.ts
+++ vite.config.ts
@@ -26,7 +26,7 @@ export default defineConfig( {
 		// https://vitejs.dev/guide/build#library-mode
 		lib: {
 			entry: resolve( __dirname, 'src/index.ts' ),
-			name: 'CKEditor',
+			name: 'CKEDITOR_REACT',
 			fileName: 'index'
 		},
 
