d6955fe4cfc93f24d9b861c7aba9c18920796055
Mateusz Baginski <cziken58@gmail.com>
2024-10-04 15:02:37 +0200

    Enforce cleanups twice

--- vitest-setup.ts
+++ vitest-setup.ts
@@ -5,6 +5,7 @@
 
 import '@testing-library/jest-dom/vitest';
 import { cleanup } from '@testing-library/react';
-import { beforeEach } from 'vitest';
+import { beforeEach, afterEach } from 'vitest';
 
 beforeEach( cleanup );
+afterEach( cleanup );
