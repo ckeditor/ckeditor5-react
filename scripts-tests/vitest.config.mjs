122ccef0dd60498dc6379e8000025e29c069aeda
Przemysław Zań <przemyslaw.zan@gmail.com>
2025-12-08 12:01:36 +0100

    Fixed config.

--- scripts-tests/vitest.config.mjs
+++ scripts-tests/vitest.config.mjs
@@ -1,6 +1,6 @@
 /**
  * @license Copyright (c) 2003-2025, CKSource Holding sp. z o.o. All rights reserved.
- * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-licensing-options
+ * For licensing, see LICENSE.md.
  */
 
 import { defineConfig } from 'vitest/config';
@@ -11,15 +11,15 @@ export default defineConfig( {
 		mockReset: true,
 		restoreMocks: true,
 		include: [
-			'./scripts-tests/**/*.@(js|mjs|cjs)'
+			'scripts-tests/**/*.@(js|mjs|cjs)'
 		],
 		exclude: [
-			'./scripts-tests/vitest.config.mjs'
+			'scripts-tests/vitest.config.mjs'
 		],
 		coverage: {
 			provider: 'v8',
 			include: [
-				'./scripts-tests/**'
+				'scripts/**'
 			],
 			reporter: [ 'text' ]
 		}
