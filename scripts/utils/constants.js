47a957c0aed0162840e955c32b12dfa7f666ade3
Filip Sobol <filipsobol@users.noreply.github.com>
2025-12-15 11:40:45 +0100

    Enforce the `node:` prefix in imports.

--- scripts/utils/constants.js
+++ scripts/utils/constants.js
@@ -3,7 +3,7 @@
  * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-licensing-options
  */
 
-import { fileURLToPath } from 'url';
+import { fileURLToPath } from 'node:url';
 import upath from 'upath';
 
 const __filename = fileURLToPath( import.meta.url );
